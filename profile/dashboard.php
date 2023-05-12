177bd10a5aed60e8cbbfd58236beb1dc5aed688f
rafiqul0004 <kr.rafiqul@gmail.com>
2023-05-13 02:23:10 +0600

    profile

---
+++ profile/dashboard.php
@@ -0,0 +1,34 @@
+<?php
+//include auth_session.php file on all user panel pages
+include("auth_session.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - Client area</title>
+    <link rel="stylesheet" href="style.css" />
+    <style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: blue;
+}
+</style>
+</head>
+<body>
+    <div class="form">
+        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+        <p>You are in user dashboard page.</p>
+        <a href="To_Do/index.html" target="_blank">Make Your To-Do List</a>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
